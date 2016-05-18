88061c4b5cbf7e5ff3d897b98ed0d0e55bacccf8
henrytriplette <henry@triplette.it>
2016-05-18 10:07:39 +0200

    Added colors to config_inc
    Added colors to config_inc.php

--- config_inc.php
+++ config_inc.php
@@ -7,3 +7,17 @@ $g_db_password = '';
 
 # --- Custom Theme ---
 $g_css_include_file = "/css/custom/default.css";
+
+# --- Status color additions ---
+
+$g_status_colors['new'] = '#ff9800';
+$g_status_colors['feedback'] = '#ff5722';
+$g_status_colors['acknowledged'] = '#795548';
+$g_status_colors['confirmed'] = '#607d8b';
+$g_status_colors['assigned'] = '#2196f3';
+$g_status_colors['resolving'] = '#e91e63';
+$g_status_colors['resolving_standby'] = '#673ab7';
+$g_status_colors['readytotest'] = '#cddc39';
+$g_status_colors['testing'] = '#8bc34a';
+$g_status_colors['resolved'] = '#4caf50';
+$g_status_colors['closed'] = '#9e9e9e';
